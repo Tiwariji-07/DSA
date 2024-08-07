aed8b50c685f69f96801c933784a6f16f671fb00
Tiwariji-07 <vivekr_500340@WMHYLTAP052.local>
2024-08-07 12:41:45 +0530

    Initial commit: Setup the environment

---
+++ code.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class code {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        System.out.println("Number:"+n);
+        scanner.close();
+    }
+}
